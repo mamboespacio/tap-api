e7055bbf6b6233d5c0ec13f25b8ff43c35c85411
mamboespacio <emadobao+1@gmail.com>
2025-01-10 09:21:45 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,41 @@
+import express from 'express';
+import cors from 'cors';
+import { MercadoPagoConfig, Preference } from 'mercadopago';
+
+const client = new MercadoPagoConfig({
+  accessToken: process.env.MP_ACCESS_TOKEN,
+});
+
+const app = express();
+const port = 3000;
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.post('/create_preference', async (req, res) => {
+  try {
+    const body = {
+      items: [
+        {
+          title: 'Dummy Product',
+          unit_price: 100,
+          quantity: 1,
+          currency_id: 'ARS',
+        }
+      ]
+    }
+    const preference = new Preference(client);
+    const response = await preference.create({ body });
+    res.json(response);
+  }
+  catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
