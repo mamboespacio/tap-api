ddbed09e0f95831d0bbfc48055604e0ef215276f
mamboespacio <emadobao+1@gmail.com>
2025-01-10 15:19:08 -0300

    config CORS

--- index.js
+++ index.js
@@ -8,15 +8,12 @@ const client = new MercadoPagoConfig({
 
 const app = express();
 const port = 3000;
-app.use(cors());
+const corsOptions = {
+  origin: '*',
+  optionsSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 app.use(express.json());
-app.use((res, next) => {
-  // res.setHeader("Access-Control-Allow-Origin", process.env.ORIGIN_URL);
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  next();
-})
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
