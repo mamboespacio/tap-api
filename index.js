66f748e095a8714912fbfd0d1959628acd4d03dc
mamboespacio <emadobao+1@gmail.com>
2025-01-10 09:36:41 -0300

    add cors headers

--- index.js
+++ index.js
@@ -10,6 +10,12 @@ const app = express();
 const port = 3000;
 app.use(cors());
 app.use(express.json());
+app.use((res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", process.env.ORIGIN_URL);
+  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+})
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
