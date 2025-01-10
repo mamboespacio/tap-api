67943ac3696075c75b2c0c2f7a9be07cd20f8da7
mamboespacio <emadobao+1@gmail.com>
2025-01-10 15:21:42 -0300

    config CORS try2

--- index.js
+++ index.js
@@ -12,8 +12,18 @@ const corsOptions = {
   origin: '*',
   optionsSuccessStatus: 200,
 };
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
