6883388604948b620a7704eb52efe363e6a9d81e
mamboespacio <emadobao+1@gmail.com>
2025-12-03 09:36:58 -0300

    expose database variables during compilation

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
-const nextConfig = {  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Configuración de env para asegurar que Vercel las use durante el build time
+  env: {
+    DATABASE_URL: process.env.DATABASE_URL,
+    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY,
+    // Añade cualquier otra variable que necesites aquí
+  },
 };
 
 export default nextConfig;
