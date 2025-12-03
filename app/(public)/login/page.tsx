29bd18b33986362a833291287434e941d9c25fc2
mamboespacio <emadobao+1@gmail.com>
2025-12-03 10:37:40 -0300

    add missing form input

--- app/(public)/login/page.tsx
+++ app/(public)/login/page.tsx
@@ -2,13 +2,15 @@
 "use client";
 
 import { Suspense } from 'react';
-import { useLoginForm } from "@/hooks/use-login-form"; // Importamos el nuevo hook
+import LoginFormContainer from '@/components/LoginForm';
 
 export default function LoginPage() {
   return (
     <div className="max-w-md mx-auto p-6 space-y-6">
       <h1 className="text-2xl font-semibold">Iniciar Sesión</h1>
-      <Suspense fallback={<div>Cargando formulario de login...</div>}></Suspense>
+      <Suspense fallback={<div>Cargando formulario de login...</div>}>
+       <LoginFormContainer/>
+      </Suspense>
       <p className="text-sm">
         ¿No tenés cuenta?{" "}
         <a className="underline" href="/register">
